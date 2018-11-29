13cb9d4f4118cc263a3954d55c1da78d6f55cae8
Benoit Pasquier <pasquieb@uci.edu>
2018-11-28 19:07:53 -0800

    WIP

---
+++ src/Inpaintings.jl
@@ -0,0 +1,8 @@
+module Inpaintings
+# This module translates the `inpaint_nans` MATLAB code from John d'Erico
+
+# using SparseArrays, SuiteSparse, LinearAlgebra
+
+include("inpaint_nans.jl")
+
+end # module
