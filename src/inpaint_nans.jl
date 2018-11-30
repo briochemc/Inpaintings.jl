860f3eea7f4d5f6fe501b1fc4dce407346630de8
Benoit Pasquier <pasquieb@uci.edu>
2018-11-30 14:05:20 -0800

    Added diagonally-full Laplacian

--- src/inpaint_nans.jl
+++ src/inpaint_nans.jl
@@ -8,7 +8,11 @@ Currently only method `0` is implemented.
 """
 function inpaint_nans(A, method=0)
     # TODO add other methods from John d'Errico's MATLAB's `inpaint_nans`.
-    return inpaint_nans_method0(A)
+    return @match method begin
+        0 => inpaint_nans_method0(A)
+        6 => inpaint_nans_method6(A)
+        _ => error("Method $method not available yet. Suggest it on Github!")
+    end
 end
 
 
@@ -98,11 +102,75 @@ function inpaint_nans_method0(A::Array{T,2}) where T
     n2 = LinearIndices(size(A))[first(R) + N2] - LinearIndices(size(A))[first(R)]
 
     # Build the Laplacian (not the fastest way but easier-to-read code)
-    Δ =  sparse(u, iwork                , -4.0, nw, length(A))
+    Δ =  sparse(u, iwork      , -4.0, nw, length(A))
     Δ += sparse(u, iwork .- n1, +1.0, nw, length(A))
     Δ += sparse(u, iwork .+ n1, +1.0, nw, length(A))
     Δ += sparse(u, iwork .- n2, +1.0, nw, length(A))
     Δ += sparse(u, iwork .+ n2, +1.0, nw, length(A))
+
+    # Use Linear indices to access the sparse Laplacian
+    inan = LinearIndices(size(A))[Inan]
+    inotnan = LinearIndices(size(A))[Inotnan]
+
+    # knowns to right hand side
+    rhs = -Δ[:, inotnan] * A[inotnan]
+
+    # and solve...
+    B = copy(A)
+    B[inan] .= Δ[:, inan] \ rhs
+    return B
+end
+
+"""
+    inpaint_nans_method6(A::Array{T,2}) where T
+
+Inpaints `NaN` values by solving a diffusion PDE for a diagonally filled Laplacian:
+```math
+\mathbf{D}_{xy}^{2} = \begin{bmatrix}0.25&0.5&0.25\\0.5&-3&0.5\\0.25&0.5&0.25\end{bmatrix}
+```
+Inspired by MATLAB's `inpaint_nans`'s method `0` for matrices (by John d'Errico).
+See https://www.mathworks.com/matlabcentral/fileexchange/4551-inpaint_nans.
+"""
+function inpaint_nans_method6(A::Array{T,2}) where T
+    Inan = findall(@. isnan(A))
+    Inotnan = findall(@. !isnan(A))
+
+    # direct and diagonal neighbors
+    N0 = CartesianIndex(0, 0)
+    N1 = CartesianIndex(1, 0)
+    N2 = CartesianIndex(0, 1)
+    neighbors = sort([n1 + n2 for n1 in (-N1, N0, N1), n2 in (-N2, N0, N2) if n1 + n2 ≠ N0])
+
+    # list of strict neighbors
+    R = CartesianIndices(size(A))
+    Ineighbors = list_neighbors(R, Inan, neighbors)
+
+    # list of all nodes we have identified
+    Iwork = [Inan; Ineighbors]
+    # Remove borders (Von-Neuman boundary)
+    R₂ = CartesianIndices(size(A) .- 2)
+    R₂ = [r₂ + first(R₂) for r₂ in R₂]
+    Iwork = [w for w in Iwork if w ∈ R₂]
+    # Sort it (not sure the sorting is required)
+    Iwork = sort(Iwork)
+    nw = length(Iwork)
+    u = collect(1:nw)
+
+    # Use Linear indices to generate the sparse Laplacian
+    iwork = LinearIndices(size(A))[Iwork]
+    n1 = LinearIndices(size(A))[first(R) + N1] - LinearIndices(size(A))[first(R)]
+    n2 = LinearIndices(size(A))[first(R) + N2] - LinearIndices(size(A))[first(R)]
+
+    # Build the Laplacian (not the fastest way but easier-to-read code)
+    Δ =  sparse(u, iwork      , -3.0, nw, length(A))
+    Δ += sparse(u, iwork .- n1, +0.5, nw, length(A))
+    Δ += sparse(u, iwork .+ n1, +0.5, nw, length(A))
+    Δ += sparse(u, iwork .- n2, +0.5, nw, length(A))
+    Δ += sparse(u, iwork .+ n2, +0.5, nw, length(A))
+    Δ += sparse(u, iwork .- (n1 + n2), +0.25, nw, length(A))
+    Δ += sparse(u, iwork .+ (n1 + n2), +0.25, nw, length(A))
+    Δ += sparse(u, iwork .- (n1 - n2), +0.25, nw, length(A))
+    Δ += sparse(u, iwork .+ (n1 - n2), +0.25, nw, length(A))
 
     # Use Linear indices to access the sparse Laplacian
     inan = LinearIndices(size(A))[Inan]
