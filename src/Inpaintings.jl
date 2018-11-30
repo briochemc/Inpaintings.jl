bd3359d6bd8471decc82f20debcc45ff14e15d1f
Benoit Pasquier <pasquieb@uci.edu>
2018-11-30 14:08:48 -0800

    Added Match dep

--- src/Inpaintings.jl
+++ src/Inpaintings.jl
@@ -2,6 +2,7 @@ module Inpaintings
 # This module translates the `inpaint_nans` MATLAB code from John d'Erico
 
 using SparseArrays, SuiteSparse, LinearAlgebra
+using Match
 
 include("inpaint_nans.jl")
 
